e42a5fd1c841ba75a42c6b632b90a77d9f95aa3e
Ankit0085d <ankitbhau563@gmail.com>
2026-05-29 14:47:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ OWNER_ID = int(getenv("OWNER_ID", 7574330905))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://pusers:nycreation@nycreation.pd4klp1.mongodb.net/?retryWrites=true&w=majority&appName=NYCREATION")
 SUPPORT_GRP = getenv("SUPPORT_GRP", "ShrutiBotSupport")
 UPDATE_CHNL = getenv("UPDATE_CHNL", "ShrutiBots")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "WTF_WhyMeeh")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "sexstake")
 
 # Random Start Images
 IMG = [
